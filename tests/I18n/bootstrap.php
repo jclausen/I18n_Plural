4a0b0bb0fcc85425e99bc4980a88610609e1a1fc
Jon Clausen <jon_clausen@silowebworks.com>
2014-05-20 10:55:10 -0400

    switch I18n submodule to Kohana 3.3 branch

--- tests/I18n/bootstrap.php
+++ tests/I18n/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
-// Initialize autoloader
-require_once __DIR__.'/loader.php';
+// Load Kohana bootstrap
+require __DIR__.'/kohana.php';
+
+// Initialize our own autoloader
+require_once DOCROOT.'loader'.EXT;
 spl_autoload_register(array(
-	new I18n\Tests\Autoloader(array(__DIR__.'/../', __DIR__.'/../../classes/')),
+	new I18n\Tests\Autoloader(array(DOCROOT.'../')),
 	'load',
 ));
 
 // Initialize helper classes
-require_once __DIR__.'/helpers.php';
+require_once DOCROOT.'helpers'.EXT;
